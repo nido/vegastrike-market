438f6d7f0f5d18b286fa0c1d4027a9126e8f9a58
Nido Media <nido@foxserver.be>
2013-09-14 23:37:15 +0200

    Consumer isolated

---
+++ Consumer.hpp
@@ -0,0 +1,27 @@
+#ifndef H_CONSUMER
+#define H_CONSUMER
+#include "scew/types.h"
+class Consumer;
+#include "factory.hpp"
+
+class Consumer{
+friend class Economy;
+public:
+    Consumer(double capital, Economy *economy, Products consumption, string name,double subsidyrate);
+
+    void tick();
+
+    ~Consumer();
+
+	void addElement(scew_element* root) const;
+    static Consumer *consumerFromElement(scew_element* root,Economy *economy,string name);
+
+private:
+    string name;
+    double capital;
+    Economy *economy;
+    Products consumption;
+    double subsidyrate;
+};
+
+#endif
