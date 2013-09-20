50b456a5c6b67a2a6ac37d3f51b9406703fea79d
Nido Media <nido@foxserver.be>
2013-09-20 23:00:50 +0200

    made doxygen aware of vectors

---
+++ doc/doxygen_stl.cpp
@@ -0,0 +1,6 @@
+/** Definitions for Doxygen so it knows of STL stuff and make proper diagrams.
+ */
+namespace std {
+  /*! STL vector class */
+  template<class T> class vector { public: T element; };
+}
