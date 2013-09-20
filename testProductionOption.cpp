de62318cbf190309092f0f2bf11c9c988c4780f4
Nido Media <nido@foxserver.be>
2013-09-20 02:49:17 +0200

    added test

--- testProductionOption.cpp
+++ testProductionOption.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <assert.h>
 
 #include "images.h"
 #include "ProductionOption.hpp"
@@ -6,22 +7,25 @@
 #define TURNS 200000
 
 int main(void){
-	std::vector<Cargo> *stuff1 = new std::vector<Cargo>();
-	std::vector<Cargo> *stuff2 = new std::vector<Cargo>();
+	std::vector<Cargo> stuff1 = std::vector<Cargo>();
+	std::vector<Cargo> stuff2 = std::vector<Cargo>();
 	std::vector<Cargo> *stuff3 = new std::vector<Cargo>();
 
-	stuff1->push_back(Cargo("input", "", 0.0, 1, 0.0, 0.0, 0.0, 0.0));
-	stuff2->push_back(Cargo("output", "",0.0, 1, 0.0, 0.0, 0.0, 0.0));
+	stuff1.push_back(Cargo("input", "", 0.0, 1, 0.0, 0.0, 0.0, 0.0));
+	stuff2.push_back(Cargo("output", "",0.0, 1, 0.0, 0.0, 0.0, 0.0));
 	stuff3->push_back(Cargo("input", "",0.0, TURNS, 0.0, 0.0, 0.0, 0.0));
 
-	ProductionOption* o = new ProductionOption(*stuff1, *stuff2);
+	ProductionOption* o = new ProductionOption(stuff1, stuff2);
 
-	for(int i=0; i<TURNS; i++) {
+	Cargo* in = findCargo("input", stuff3);
+	Cargo* out = findCargo("output", stuff3);
+
+	for(int i=1; i <= TURNS; i++) {
 		o->Produce(stuff3);
+		assert(in->quantity == TURNS - i);
+		assert(out->quantity == i);
 	}
 
-	delete stuff1;
-	delete stuff2;
 	delete stuff3;
 	delete o;
 
