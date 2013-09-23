d146a8d0158bb78959d1bd3889dc0731dead99cd
Nido Media <nido@foxserver.be>
2013-09-24 00:33:58 +0200

    made suites selectable and made use of that in the coverage reports

--- test/cppunittest.cpp
+++ test/cppunittest.cpp
@@ -4,19 +4,29 @@
 #include <cppunit/TestSuite.h>
 #include <cppunit/ui/text/TestRunner.h>
 
+#include <string.h>
+
 #include "CargoTest.hpp"
 #include "CargoHoldTest.hpp"
 #include "ProductionOptionTest.hpp"
 #include "FactoryTest.hpp"
 
-int main(void){
+int main(int argc, char* argv[]){
 	CppUnit::TestSuite suite;
 	CppUnit::TextUi::TestRunner runner;
 
-	runner.addTest( CargoTest::suite() );
-	runner.addTest( CargoHoldTest::suite() );
-	runner.addTest( ProductionOptionTest::suite() );
-	runner.addTest( FactoryTest::suite() );
+	if (argc == 1 || strcmp(argv[1], "cargo") == 0){
+		runner.addTest( CargoTest::suite() );
+	}
+	if (argc == 1 || strcmp(argv[1], "cargohold") == 0){
+		runner.addTest( CargoHoldTest::suite() );
+	}
+	if (argc == 1 || strcmp(argv[1], "productionoption") == 0){
+		runner.addTest( ProductionOptionTest::suite() );
+	}
+	if (argc == 1 || strcmp(argv[1], "factory") == 0){
+		runner.addTest( FactoryTest::suite() );
+	}
 
 	runner.run();
 	return 0;
