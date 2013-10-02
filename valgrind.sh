f2d6721f19f3f78994ab59e13c6ca6f340da6537
Nido Media <nido@foxserver.be>
2013-10-03 00:07:15 +0200

    removed extra tool calls and made call configurable

--- valgrind.sh
+++ valgrind.sh
@@ -1,8 +1,13 @@
-valgrind --tool=callgrind --callgrind-out-file=unittest.callgrind ./cppunittest
-gprof2dot -f callgrind -o unittest-callgrind.dot unittest.callgrind
-dot -Tpdf -ounittest-callgrind.pdf unittest-callgrind.dot
-dot -Tpng -ounittest-callgrind.png unittest-callgrind.dot
+#!/bin/bash -x
+COMMAND=$1
+TESTNAME=$(basename $1)
 
-valgrind --tool=cachegrind --branch-sim=yes --cachegrind-out-file=unittest.cachegrind ./cppunittest
-valgrind --tool=massif --peak-inaccuracy=0 --stacks=yes --massif-out-file=unittest.massif ./cppunittest 
+valgrind --tool=callgrind --callgrind-out-file=${TESTNAME}.callgrind ${COMMAND} 10
+gprof2dot -f callgrind -o ${TESTNAME}.dot ${TESTNAME}.callgrind
+dot -Tpdf -o${TESTNAME}.pdf ${TESTNAME}.dot
+dot -Tsvg -o${TESTNAME}.svg ${TESTNAME}.dot
+dot -Tpng -o${TESTNAME}.png ${TESTNAME}.dot
+
+#valgrind --tool=cachegrind --branch-sim=yes --cachegrind-out-file=unittest.cachegrind ./cppunittest
+#valgrind --tool=massif --peak-inaccuracy=0 --stacks=yes --massif-out-file=unittest.massif ./cppunittest 
 
