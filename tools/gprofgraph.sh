7f6512b0b6ff86e7f93740349c901e999ffbc602
Nido Media <nido@foxserver.be>
2013-10-06 02:43:18 +0200

    altered graphing tools performance test type bla

--- tools/gprofgraph.sh
+++ tools/gprofgraph.sh
@@ -11,7 +11,7 @@ cd gprof
 #TODO: cmake with -pg
 cmake -DCMAKE_BUILD_TYPE=Release -D"CMAKE_CXX_FLAGS:string=-pg" ..
 make
-./bigtest
+./bigtest 100 30 1000
 gprof bigtest gmon.out > bigtest.gprof
 gprof2dot -s bigtest.gprof -o bigtest.dot
 dot -Tpng -o ../bigtest-gprof.png bigtest.dot
--- tools/valgrindgraph.sh
+++ tools/valgrindgraph.sh
@@ -15,6 +15,6 @@ cd valgrind
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 
-valgrind --tool=callgrind --callgrind-out-file=${TESTNAME}.callgrind ${COMMAND} 10
+valgrind --tool=callgrind --callgrind-out-file=${TESTNAME}.callgrind ${COMMAND} 10 30 1000
 gprof2dot -s -f callgrind -o ${TESTNAME}-callgrind.dot ${TESTNAME}.callgrind
 dot -Tpng -o../${TESTNAME}-callgrind.png ${TESTNAME}-callgrind.dot
