969da9530fac37a82adae283384aefc9012e671a
Nido Media <nido@foxserver.be>
2013-10-07 00:40:24 +0200

    added guards and removed unnecesary include of <map>

--- src/XMLParse.hpp
+++ src/XMLParse.hpp
@@ -1,5 +1,9 @@
+#ifndef H_XMLPARSE
+#define H_XMLPARSE
+
 #include <string>
 #include <vector>
+#include <map>
 
 #include <expat.h>
 
@@ -10,4 +14,4 @@ void XMLParseElement(void *userData, const XML_Char *name, const XML_Char **atts
 void ParseCargoType(CargoType* output, const XML_Char **atts );
 void XMLCargoTypeVectorBegin( void *userData, const XML_Char *name, const XML_Char **atts );
 std::vector<CargoType> readCargoTypes(std::string filename);
-
+#endif //H_XMLPARSE
