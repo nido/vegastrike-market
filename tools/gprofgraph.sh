bdf7fa1c8a4ba7cdd77ac96d71a38bd743eab8d3
Nido Media <nido@foxserver.be>
2013-10-20 21:11:11 +0200

    fixed that gprof uses the gprof file actually existing

--- tools/gprofgraph.sh
+++ tools/gprofgraph.sh
@@ -9,10 +9,10 @@ test -d gprof || mkdir gprof
 cd gprof
 
 #TODO: cmake with -pg
-cmake -DCMAKE_BUILD_TYPE=Release -D"CMAKE_CXX_FLAGS:string=-pg" ..
-make
+cmake -DCMAKE_BUILD_TYPE=Release -D"CMAKE_CXX_FLAGS=-p -pg" ..
+make VERBOSE=1
 ./bigtest 100 30 1000
-gprof bigtest gmon.out > bigtest.gprof
+gprof bigtest ../data/gmon.out > bigtest.gprof
 gprof2dot -s bigtest.gprof -o bigtest.dot
 dot -Tpng -o ../bigtest-gprof.png bigtest.dot
 
