67bfa36beaf78edce02ffc98e5fc4a7e3e8e0d4f
Nido Media <nido@foxserver.be>
2013-10-20 21:51:44 +0200

    commented Factory

--- src/Factory.hpp
+++ src/Factory.hpp
@@ -12,35 +12,53 @@ public:
   /** Create an empty Factory */
   Factory();
 
-  /** copy constructor */
+  /** copy constructor
+   * @param that the Factory to copy
+   */
   Factory(const Factory& that);
 
   /** delete a factory */
   ~Factory();
 
-  /** (if possible) Do producuce */
+  /** (if possible) Do producuce 
+   * @param cargoStore the Cargo to take cargo from and put cargo in
+   */
   void Produce(Cargo& cargoStore) const;
 
-  /** Check if production is possible */
+  /** Check if production is possible
+   * @param cargoStore the Cargo from which to make this assessment
+   */
   bool canProduce(const Cargo& cargoStore) const;
 
-  /** Add another production option */
+  /** Add another production option
+   * @param option the ProductionOption to add to the factory
+   */
   void addProductionOption(const ProductionOption& option);
 
   /** Set the active production option.
  	 * Note this one needs to be part of the Factory's own ProductionOptions
+ 	 * @param option the Option to use in Production
  	 */
   void setProductionOption(const ProductionOption& option);
 
   /** Compare factories to eachother. Factories are equal if the
- 	 * have exactly the same ProductionOptions.
- 	 */
+   * have exactly the same ProductionOptions.
+   * @param that the Factory to compare to
+   * @return true when the factories are equal, false otherwise
+   */
   bool operator==(const Factory &that) const;
 
+  /** Assign a Factory to this one
+   * @param the Factory to copy
+   * @return pointer to this factory
+   */
   Factory& operator=(const Factory& that);
 
 private:
+  /** list of possible ProductionOptions */
   std::vector<ProductionOption> options;
+
+  /** ProductionOption used on Production */
   std::vector<ProductionOption>::iterator productionPlan;
 };
 
