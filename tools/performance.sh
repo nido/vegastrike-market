d4033e079d0f91ca9242f08bf9c4a4d3f690d604
Nido Media <nido@foxserver.be>
2013-10-21 14:05:24 +0200

    performance measure tool

--- tools/performance.sh
+++ tools/performance.sh
@@ -8,13 +8,22 @@ test -f  CMakeCache.txt && rm CMakeCache.txt && echo "warning: removed CMakeCach
 test -d performance || mkdir performance
 cd performance
 
-cmake -D"CMAKE_BUILD_TYPE=Release" -D"CMAKE_CXX_COMPILER=g++" ..
-cmake -D"CMAKE_BUILD_TYPE=Release" ..
-make VERBOSE=1
-/usr/bin/time -f "timing gcc %e" ./bigtest 10 100 1000
 
-cmake -DCMAKE_BUILD_TYPE=Release "-DCMAKE_CXX_COMPILER=clang++" ..
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make VERBOSE=1
-/usr/bin/time -f "timing clang %e" ./bigtest 10 100 1000
+for compiler in g++ clang++;
+do
 
+cmake -D"CMAKE_BUILD_TYPE=Release" -D"CMAKE_CXX_COMPILER=${compiler}" ..
+make clean
+make bigtest
+rm ${compiler}timing
+for x in `seq 0 10 100`; do
+for y in `seq 0 100 1000`; do
+echo bigtest 10 $x $y
+/usr/bin/time -a -o ${compiler}timing -f "${x}	${y}	%e	%M	%K	%D	%F	%U	%R	%W" ./bigtest 10 ${x} ${y} >/dev/null
+
+done
+done
+echo "set terminal svg; set output '../time-${compiler}.svg'; set dgrid3d; splot '${compiler}timing' using 1:2:3 with lines" | gnuplot
+echo "set terminal svg; set output '../memory-${compiler}.svg'; set dgrid3d; splot '${compiler}timing' using 1:2:4 with lines" | gnuplot
+
+done
