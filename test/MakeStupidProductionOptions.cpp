d746535e7e5e1a8d9bd2341d68a33ed4882755c3
Nido Media <nido@foxserver.be>
2015-03-30 01:29:35 +0200

    created a script that can make production options for testing, fixed up current xml, added productionoption xml

---
+++ test/MakeStupidProductionOptions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "ProductionOption.hpp"
+#include "MPLParse.hpp"
+#include "XMLNode.hpp"
+
+int main(int argc, char* argv[])
+{
+  MPLParse parser = MPLParse();
+  std::vector<CargoType> cts = parser.ParseFile("../data/master_part_list.csv");
+  std::vector<ProductionOption> pos;
+  for (std::vector<CargoType>::iterator i = cts.begin(); i != cts.end(); ++i){
+    if (i + 1 != cts.end()){
+      CargoType t = *i;
+      CargoType q = *(i+1);
+      Cargo in, out;
+      in.addCargo(t, 1);
+      out.addCargo(t, 2);
+      ProductionOption p = ProductionOption(in, out);
+      XMLNode n = XMLNode(p);
+      std::cout<<n.getXML()<<std::endl;
+      pos.push_back(p);
+    }
+  }
+  std::cout<<pos.size()<<std::endl;
+}
