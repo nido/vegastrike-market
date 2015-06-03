0a384159343dd42fe0e4039ff009eb3cea1ae24a
Nido Media <nido@foxserver.be>
2015-06-03 19:31:38 +0200

    dec++11ized for windows

--- src/Cargo.cpp
+++ src/Cargo.cpp
@@ -81,7 +81,7 @@ bool Cargo::contains(const Cargo &newCargo) const {
     if (this->cargo.find(newStock->first) == this->cargo.end()) {
       return false;
     }
-    if (this->cargo.at(newStock->first) < newStock->second) {
+    if (this->cargo.find(newStock->first)->second< newStock->second) {return false;}
       return false;
     }
   }
